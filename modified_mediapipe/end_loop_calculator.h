00f2fe6ac9dc844f5c2d20ae76c06029fd2f4f27
Anna Kim <48214314+rabBit64@users.noreply.github.com>
2020-01-18 16:08:17 +0900

    Update end_loop_calculator.h

--- modified_mediapipe/end_loop_calculator.h
+++ modified_mediapipe/end_loop_calculator.h
@@ -1,4 +1,5 @@
 // Copyright 2019 The MediaPipe Authors.
+// Modifications copyright (C) 2020 <Argo/jongwook>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
